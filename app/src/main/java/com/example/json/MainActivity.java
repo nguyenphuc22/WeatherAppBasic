393f2b13955a56e71d4024a1b6454b9c25c06be2
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2020-11-26 17:17:39 +0700

    Source Code

---
+++ app/src/main/java/com/example/json/MainActivity.java
@@ -0,0 +1,201 @@
+package com.example.json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    String City = "London";
+    //Your Key
+    String Key = "2a20d2a71c5a0737f88861d43949d630";
+
+    String url1 = "https://samples.openweathermap.org/data/2.5/weather?q=London&appid=439d4b804bc8187953eb36d2a8c26a02";
+
+    String url = "https://api.openweathermap.org/data/2.5/weather?q=" + City +"&units=metric&appid=" + Key;
+
+    TextView txtCity,txtTime,txtValue,txtValueFeelLike,txtValueHumidity,txtValueVision;
+
+    String nameIcon = "10d";
+
+
+
+    ImageView imgIcon;
+
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bitmap = null;
+
+            URL url;
+
+            HttpURLConnection httpURLConnection;
+
+            InputStream inputStream;
+
+            try {
+                Log.i("LINK",strings[0]);
+                url = new URL(strings[0]);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                inputStream = httpURLConnection.getInputStream();
+
+                bitmap = BitmapFactory.decodeStream(inputStream);
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return bitmap;
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void , String> {
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+
+            URL url;
+
+            HttpURLConnection httpURLConnection;
+
+            InputStream inputStream;
+
+            InputStreamReader inputStreamReader;
+
+            try {
+
+                url = new URL(strings[0]);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                inputStream = httpURLConnection.getInputStream();
+
+                inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while(data != -1) {
+
+                    result += (char) data;
+
+                    data = inputStreamReader.read();
+
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtCity = findViewById(R.id.txtCity);
+
+        txtTime = findViewById(R.id.txtTime);
+
+        txtValue = findViewById(R.id.txtValue);
+
+        txtValueFeelLike = findViewById(R.id.txtTitleFeelLike);
+
+        txtValueHumidity = findViewById(R.id.txtValueHumidity);
+
+        txtValueVision = findViewById(R.id.txtValueVision);
+
+        imgIcon = findViewById(R.id.imgIcon);
+
+        DownloadTask downloadTask = new DownloadTask();
+
+        try {
+
+            String result = "abc";
+
+            result = downloadTask.execute(url).get();
+
+            Log.i("Result:",result);
+
+            JSONObject jsonObject = new JSONObject(result);
+
+            JSONObject main = jsonObject.getJSONObject("main");
+
+            String temp = main.getString("temp");
+
+            String humidity = main.getString("humidity");
+
+            String feel_like = main.getString("feels_like");
+
+            String visibility = jsonObject.getString("visibility");
+
+            nameIcon = jsonObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+
+            Log.i("Name Icon",nameIcon);
+
+            Long time = jsonObject.getLong("dt");
+
+            String sTime = new SimpleDateFormat("dd-M-yyyy hh:mm:ss", Locale.ENGLISH)
+                    .format(new Date(time * 1000));
+
+            txtCity.setText(City);
+
+            txtValue.setText(temp + "°");
+
+            txtValueVision.setText(visibility);
+
+            txtValueHumidity.setText(humidity);
+
+            txtValueFeelLike.setText(feel_like);
+
+            DownloadImage downloadImage = new DownloadImage();
+
+            String urlIcon = " https://openweathermap.org/img/wn/"+ nameIcon +"@2x.png";
+
+            Bitmap bitmap = downloadImage.execute(urlIcon).get();
+
+            imgIcon.setImageBitmap(bitmap);
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
